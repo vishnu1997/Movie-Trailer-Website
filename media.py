665c78e0b1241b79812b56c0b6b7832b574049ba
Vishnu Vardhan Reddy <reddy.vishnu1997@gmail.com>
2017-06-04 15:04:53 +0530

    Project one
    completed

---
+++ media.py
@@ -0,0 +1,11 @@
+# Movie class with 3 attributes - Movie title,poster image,movie traier Link
+
+class Movie: 
+
+    #Constructor 
+    def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
+        self.movie_title = movie_title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+#end of class
