809b4d6852796a7a27e4b3d037e4b42251d1c5a7
Vishnu Vardhan Reddy <reddy.vishnu1997@gmail.com>
2017-06-05 13:48:02 +0530

    Updated files
    Met pep8 requirements

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,16 +2,23 @@ import fresh_tomatoes
 import media
 
 # 3 Movie objects
-toy_story = media.Movie("Toy Story", "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+toy_story = media.Movie("Toy Story",
+                        "https://upload.wikimedia.org/wikipedia"
+                        "/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+avatar = media.Movie("Avatar",
+                     "https://upload.wikimedia.org/wikipedia"
+                     "/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-bahubali = media.Movie("Bahubali", "https://upload.wikimedia.org/wikipedia/en/7/7e/Baahubali_poster.jpg",
+bahubali = media.Movie("Bahubali",
+                       "https://upload.wikimedia.org/wikipedia/"
+                       "en/7/7e/Baahubali_poster.jpg",
                        "https://www.youtube.com/watch?v=sOEg_YZQsTI")
 
 # created a list of Movie objects
 movies = [toy_story, avatar, bahubali]
 
 fresh_tomatoes.open_movies_page(movies)
+
--- media.py
+++ media.py
@@ -1,16 +1,17 @@
-""" Movie class 
-
-     Attributes - 
+"""Movie class
+     Attributes -
        movie_title - Will store the title of movie,
        poster_image_url - url of the poster,
-       trailer_youtube_url - trailer of the movie link 
+       trailer_youtube_url - trailer of the movie link
 """
-class Movie: 
 
-    #Constructor 
+
+class Movie:
+
+    # Constructor
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
         self.movie_title = movie_title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
 
-#end of class
+# end of class
